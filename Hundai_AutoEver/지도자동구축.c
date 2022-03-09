35f04210cde257044258291f81e06e0d650e6f38
Ji Jae Ouk <64742982+wlwodnr2@users.noreply.github.com>
2022-03-09 12:49:44 +0900

    Update 지도자동구축.c

--- Hundai_AutoEver/지도자동구축.c
+++ Hundai_AutoEver/지도자동구축.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
+// 변 사이에 생기는 점과 중심에 생기는 점으로 정사각형 점들이 생성되고, 4개의 점이 존재한다면 그 사이에 3개의 변이 존재하기 때문에 그 다음 생기는 점의 총 개수는 4+3의 정사각형 개수이다. 
+
 int main(void)
 {
 	int N, oneLineSpot;
