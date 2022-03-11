d9a5a9941d77ae7066a583cd7c30d5a8a9815648
Ji Jae Ouk <64742982+wlwodnr2@users.noreply.github.com>
2022-03-11 10:50:13 +0900

    Create 8단변속기.c

---
+++ Hundai_AutoEver/8단변속기.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int DCT[8], step[8];
+	for (int i = 0; i < 8; i++) {
+		scanf("%d", &DCT[i]);
+	}
+
+	if (DCT[0] - DCT[1] == 1) {
+		step[0] = 1;
+	}
+	else if (DCT[0] - DCT[1] == -1) {
+		step[0] = -1;
+	}
+	else {
+		step[0] = 0;
+	}
+
+	for (int i = 1; i < 7; i++) {
+		if (DCT[i] - DCT[i + 1] == 1 && step[i - 1] == 1) {
+			step[i] = 1;
+			if (i == 6) printf("descending");
+		}
+		else if (DCT[i] - DCT[i + 1] == -1 && step[i - 1] == -1) {
+			step[i] = -1;
+			if (i == 6) printf("ascending");
+		}
+		else {
+			printf("mixed");
+			break;
+		}
+	}
+	return 0;
+}
