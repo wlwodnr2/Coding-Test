15f977e94f2ba21053cc257f484268cda7bec4b0
Ji Jae Ouk <64742982+wlwodnr2@users.noreply.github.com>
2022-04-04 16:35:55 +0900

    Create 15650(N과 M(2)).c

---
+++ 백준/Brute Force(브루트 포스)/15650(N과 M(2)).c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int N, M, check[9] = {0,}, arr[9] = {0,};
+
+void backtracking(int count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (check[i] == 0) {
+			check[i] = 1;
+			if (count == 0 || i > arr[count - 1]) {
+				arr[count] = i;
+				backtracking(count + 1);
+			}
+			check[i] = 0;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	backtracking(0);
+	return 0;
+}
